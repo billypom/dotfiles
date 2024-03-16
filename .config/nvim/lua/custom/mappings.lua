40902010b751f6538f0fe3e45b77ce477a14f278
billypom on debian <billypompey@icloud.com>
2024-03-16 17:22:17 -0400

    nvim

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,33 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+
+M.telescope = {
+  plugin = true,
+
+  n = {
+    -- find
+    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+  },
+}
+
+return M
